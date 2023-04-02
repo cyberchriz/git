c422d294e7ac87f3f1ec9bde9a6d2113bc3ef96c
chris <suer.cs@gmail.com>
2023-04-02 18:53:49 +0200

    modified:   DataScience/activation_functions.h 	modified:   DataScience/enums.h 	new file:   DataScience/sample.cpp 	modified:   DataScience/sample.h

--- DataScience/activation_functions.h
+++ DataScience/activation_functions.h
@@ -4,7 +4,36 @@
 #include <cmath>
 using namespace std;
 
-
+// enumeration of available activation functions for neural networks
+enum ACTIVATION_FUNC
+  {
+   f_ident,        // identity function
+   f_sigmoid,      // sigmoid (logistic)
+   f_ELU,          // exponential linear unit (ELU)
+   f_ReLU,         // rectified linear unit (ReLU)
+   f_LReLU,        // leaky ReLU
+   f_tanh,         // hyperbolic tangent (tanh)
+   f_oblique_tanh, // oblique tanh (custom)
+   f_tanh_rectifier,// tanh rectifier
+   f_arctan,       // arcus tangent (arctan)
+   f_arsinh,       // area sin. hyperbolicus (inv. hyperbol. sine)
+   f_softsign,     // softsign (Elliot)
+   f_ISRU,         // inverse square root unit (ISRU)
+   f_ISRLU,        // inv.squ.root linear unit (ISRLU)
+   f_softplus,     // softplus
+   f_bentident,    // bent identity
+   f_sinusoid,     // sinusoid
+   f_sinc,         // cardinal sine (sinc)
+   f_gaussian,     // gaussian
+   f_differentiable_hardstep, // differentiable hardstep
+   f_leaky_diff_hardstep, // leaky differentiable hardstep
+   f_softmax,      // normalized exponential (softmax)
+   f_oblique_sigmoid, // oblique sigmoid
+   f_log_rectifier, // log rectifier
+   f_leaky_log_rectifier, // leaky log rectifier
+   f_ramp          // ramp
+                   // note: the softmax function can't be part of this library because it has no single input but needs the other neurons of the layer, too, so it
+  };               //       needs to be defined from within the neural network code
 
 //+------------------------------------------------------------------+
 //|      return act.function as string variable equivalent           |
