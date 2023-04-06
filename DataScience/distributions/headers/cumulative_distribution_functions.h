8588ab53c088ccfd9f62232b0fd1230fe44103a6
cyberchriz <110936060+cyberchriz@users.noreply.github.com>
2023-04-06 12:21:32 +0000

    changed parameters to be passed by const ref

--- DataScience/distributions/headers/cumulative_distribution_functions.h
+++ DataScience/distributions/headers/cumulative_distribution_functions.h
@@ -17,11 +17,11 @@ class CdfObject{
                       std::is_same<T, double double>::value,
                       "T must be either float, double or double double");    
         // methods
-        static T gaussian(T x_val,T mu=0,T sigma=1);
-        static T cauchy(T x_val,T x_peak=0,T gamma=1);
-        static T laplace(T x_val,T mu=0,T sigma=1);
-        static T pareto(T x_val,T alpha=1,T tail_index=1);
-        static T lomax(T x_val,T alpha=1,T tail_index=1);
+        static T gaussian(const T& x_val, const T& mu=0, const T& sigma=1);
+        static T cauchy(const T& x_val, const T& x_peak=0, const T& gamma=1);
+        static T laplace(const T& x_val,const T& mu=0, const T& sigma=1);
+        static T pareto(const T& x_val, const T& alpha=1, const T& tail_index=1);
+        static T lomax(const T& x_val,const T& alpha=1, const T& tail_index=1);
         // constructor
         CdfObject<T>(){};
         // destructor
