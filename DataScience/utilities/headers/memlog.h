399eda7b3da1c11f5f46204126d0e4152b2e7976
cyberchriz <110936060+cyberchriz@users.noreply.github.com>
2023-04-11 17:31:38 +0000

    new utility for logging of heap allocations (overrides the 'new' operator)

---
+++ DataScience/utilities/headers/memlog.h
@@ -0,0 +1,27 @@
+// author: cyberchriz (Christian Suer)
+
+// this code logs any heap memory allocations to the console
+// by overriding the `new` operator;
+// in order to use this, simply #include this file and define the DEBUG flag as a preprocessor directive
+
+#define DEBUG
+
+#ifdef DEBUG
+#pragma once
+    #include <iostream>
+    #include <stacktrace>
+
+    // returns the name of the function with the specified level on the call stack;
+    // index 0 refers to the top level (=current function)
+    // index 1 refers to the calling function
+    const char* get_function_name(uint32_t stacklevel_index){
+        std::stacktrace trace;
+        return trace[stacklevel_index].function_name();
+    }
+
+    void* operator new(size_t size){
+        auto stackTrace = boost::stacktrace::stacktrace();   
+        std::cout << "memory heap allocation " << size << " bytes in function " << get_function_name(1) << "\n";
+        return malloc(size);
+    }
+#endif
