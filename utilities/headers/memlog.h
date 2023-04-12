91f6d127816b700b317b769fe956c7e9d3e6660c
cyberchriz <suer.cs@gmail.com>
2023-04-12 16:40:53 +0200

    fixed problems with stacktrace

--- utilities/headers/memlog.h
+++ utilities/headers/memlog.h
@@ -9,19 +9,20 @@
 #ifdef DEBUG
 #pragma once
     #include <iostream>
-    #include <stacktrace> // requires C++17
+    #include <boost/stacktrace.hpp>
 
     // returns the name of the function with the specified level on the call stack;
     // index 0 refers to the top level (=current function)
     // index 1 refers to the calling function
-    const char* get_function_name(uint32_t stacklevel_index){
-        std::stacktrace trace;
-        return trace[stacklevel_index].function_name();
+    std::string get_function_name(uint32_t stacklevel_index){
+        boost::stacktrace::stacktrace trace;
+        return trace[stacklevel_index].name();
     }
 
     void* operator new(size_t size){
         auto stackTrace = boost::stacktrace::stacktrace();   
         std::cout << "memory heap allocation " << size << " bytes in function " << get_function_name(1) << "\n";
+        std::cout.flush();
         return malloc(size);
     }
 #endif
