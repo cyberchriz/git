2384da0b56bdd30140053762cc95c3dc296c34c7
cyberchriz <suer.cs@gmail.com>
2023-04-12 02:17:09 +0200

    major file overhaul

--- DataScience/utilities/headers/array.h
+++ DataScience/utilities/headers/array.h
@@ -1,339 +1,87 @@
 #pragma once
 #include <cmath> 
+#include <memory>
 #include "sample.h"
-using namespace std;
+#include "../../distributions/headers/random_distributions.h"
 
 // class for multidimensional arrays
-
+template<typename T>
 class Array{
-    private:
-        double *data;
-        int elements;
-        int dimensions;
-        int *size;
     public:
-        int get_element(int *index);
-        void set(int *index, double value=0);
-        void set(int index, double value=0){data[index]=value;};
-        double get(int *index);
-        double get(int index){return data[index];};
-        int get_dimensions(){return dimensions;};
-        int get_size(int dimension){return size[dimension];};
-        int items(){return elements;};
-        void fill(double value=0);
-        void identity_fill();
-        void random_normal(double mu=0, double sigma=1);
-        void random_uniform(double x_mean=0.5,double range=0.5);
-        double mean(){return Sample<double>(data).mean();};
-        double median(){return Sample<double>(data).median();};
-        double variance(){return Sample<double>(data).variance();};
-        double stddev(){return Sample<double>(data).stddev();};
-        double sum();
-        void add(double value);
-        void add(Array* array);
-        double multiply();
-        void multiply(double factor);
-        void multiply(Array* arrayB);
-        void substract(double value);
-        void substract(Array* arrayB);
-        void divide(double quotient);
-        void divide(Array* arrayB);
-        void power(double exponent);
-        void root();
-        void replace(double old_value, double new_value);
-        int find(double value);
-        void map(double (*pointer_to_function)(double));
-        Array* copy();
-        Array* dotproduct(Array* arrayB);
-        Array* transpose();
-        // constructor declaration
-        Array(int *dim_size);
-        // destructor declaration
-        ~Array();
+        void set(u_int32_t &index, T value);
+        T get(u_int32_t &index);
+        u_int32_t get_dimensions(){return dimensions;};
+        u_int32_t get_size(u_int32_t dimension){return size[dimension];};
+        u_int32_t get_elements(){return elements;};
+        void fill_values(T value);
+        void fill_zeros(){fill_values(0);}
+        void fill_identity();
+        void fill_random_gaussian(T mu=0, T sigma=1);
+        void fill_random_uniform(T min=0,T max=1.0);
+        double mean(){return Sample<T>(data).mean();};
+        double median(){return Sample<T>(data).median();};
+        double variance(){return Sample<T>(data).variance();};
+        double stddev(){return Sample<T>(data).stddev();};
+        T sum();
+        void add(T value);
+        void add(const Array& array);
+        T multiply();
+        void multiply(T factor);
+        void multiply(const Array& other);
+        void substract(T value);
+        void substract(const Array& other);
+        void divide(T quotient);
+        void divide(const Array& other);
+        void pow(T exponent);
+        void sqrt();
+        void replace(T old_value, T new_value);
+        u_int32_t find(T value);
+        void function(T (*pointer_to_function)(T));
+        void operator=(const Array& other);
+        // constructor declarations
+        Array() = delete;
+        Array(u_int32_t &dim_size);
+        // destructor declarations
+        ~Array(u_int32_t &dim_size);
+    protected:
+        u_int32_t get_element(u_int32_t &index)
+        T *data; // 1dimensional array of source data
+        u_int32_t elements=0; // total number of elements in all dimensions
+        u_int32_t dimensions=0;
+        u_int32_t *size; // holds the size (number of elements) per individual dimension  
+    private:      
 };
 
-// constructor definition
-Array::Array(int *dim_size){
-    dimensions = sizeof(dim_size)/__SIZEOF_POINTER__;
-    elements=1;
-    size = new int[dimensions];
-    for (int d=0;d<dimensions;d++){
-        elements*=dim_size[d];
-        size[d]=dim_size[d];
-    }
-    data = new double[elements];
+// derived class from Array<T>,
+// for 1d vectors
+template<typename T>
+class Vector : Array<T>{
+    public:
+        void set(u_int32_t &index, T value) = delete;
+        T get(u_int32_t &index) = delete;
+        void set(u_int32_t index, T value);
+        T get(u_int32_t index);
+        // constructor declarations
+        Vector() = delete;
+        Vector(u_int32_t elements);
+        // destructor declarations
+        ~Vector(u_int32_t);
 };
 
-// destructor definition
-Array::~Array(){
-    delete size;
-    delete data;
+// derived class from Array<T>,
+// for 2d matrix
+template<typename T>
+class Matrix : Array<T>{
+    public:
+        void set(u_int32_t &index, T value) = delete;
+        T get(u_int32_t &index) = delete;
+        void set(u_int32_t index_x, u_int32_t index_y, T value);
+        T get(u_int32_t index_x, u_int32_t index_y);
+        Matrix<T> dotproduct(const Matrix& other);
+        Matrix<T> transpose();
+        // constructor declarations
+        Matrix() = delete;
+        Matrix(u_int32_t elements_x, u_int32_t elements_y);
+        // destructor declarations
+        ~Matrix(u_int32_t, u_int32_t);
 };
-
-// set a value by index
-void Array::set(int *index, double value){
-    data[get_element(index)] = value;
-};
-
-// get a array value from index
-double Array::get(int *index){
-    int element=get_element(index);
-    if (element==-1 || sizeof(index)==0){return NAN;}
-    return data[element];
-};
-
-// get 1d element index from multidimensional index
-int Array::get_element(int *index){
-    if (sizeof(index)/__SIZEOF_POINTER__ > dimensions){
-        return -1;
-    }
-    int result=index[0];
-    // int result=index[0] + index[1]*size[0] + index[2]*size[0]*size[1] + index[3]*size[0]*size[1]*size[2] ...;
-    for (int d=1;d<dimensions;d++){
-        int add=index[d];
-        for(int s=d;s>0;s--){
-            add*=size[s-1];
-        }
-        result+=add;
-    }
-    return result;
-};
-
-// fill entire array with given value
-void Array::fill(double value){
-    for (int i=0;i<elements;i++){
-        data[i]=value;
-    }
-};
-
-// fill array with identity matrix
-// (works with array of up to 4 dimensions)
-void Array::identity_fill(){
-    if(dimensions==1){this->fill(1);}
-    else if (dimensions==2){
-        for (int x=0;x<this->size[0];x++){
-            for (int y=0;y<this->size[1];y++){
-                int index[]={x,y};
-                data[this->get_element(index)] = x==y;
-            }
-        }
-    }
-    else if (dimensions==3){
-        for (int x=0;x<this->size[0];x++){
-            for (int y=0;y<this->size[1];y++){
-                for (int z=0;z<this->size[2];z++){
-                    int index[]={x,y,z};
-                    data[this->get_element(index)] = x==y && y==z;
-                }
-            }
-        }
-    }
-    else if (dimensions==4){
-        for (int x=0;x<this->size[0];x++){
-            for (int y=0;y<this->size[1];y++){
-                for (int z=0;z<this->size[2];z++){
-                    for (int a=0;a<this->size[3];a++){
-                        int index[]={x,y,z,a};
-                        data[this->get_element(index)] = x==y && y==z && z==a;
-                    }
-                }
-            }
-        }
-    }    
-}
-
-// fill with random normal distribution
-void Array::random_normal(double mu,double sigma){
-    for (int i=0;i<elements;i++){
-        double random=(double)rand() / RAND_MAX;                    // random value within range 0-1
-        random/=sqrt(2*M_PI*pow(sigma,2));                          // reduce to the top of the distribution (f(x_val=mu))
-        double algsign=1;if (rand()>(0.5*RAND_MAX)){algsign=-1;}    // get random algebraic sign
-        data[i] = algsign * (mu + sigma * sqrt (-2 * log (random / (1/sqrt(2*M_PI*pow(sigma,2))))));
-    }
-};
-
-// fill with random uniform distribution
-void Array::random_uniform(double x_mean,double range){
-    for (int i=0;i<elements;i++){
-        double random=(double)rand() / (0.5*RAND_MAX) - 1;          // random value within range +/- 1
-        random*=range;
-        random+=x_mean;
-        data[i] = random;
-    }
-};
-
-// modifies the array by applying the referred function (should take a single double as argument)
-// template: Array::map(&func);
-void Array::map(double (*pointer_to_function)(double)){
-    for (int i=0;i<elements;i++){
-        data[i]=pointer_to_function(data[i]);
-    }
-};
-
-// returns the sum of all array elements
-double Array::sum(){
-    double result=0;
-    for (int i=0;i<elements;i++){
-        result+=data[i];
-    }
-    return result;
-}
-
-// returns the product of all array elements
-double Array::multiply(){
-    double result = data[0];
-    for (int i=1;i<elements;i++){
-        result*=data[i];
-    }
-    return result;
-}
-
-// elementwise (scalar) multiplication by given factor
-void Array::multiply(double factor){
-    for (int i=0;i<elements;i++){
-        data[i]*=factor;
-    }
-}
-
-// elementwise (scalar) division by given quotient
-void Array::divide(double quotient){
-    if (quotient==0){return;}
-    for (int i=0;i<elements;i++){
-        data[i]/=quotient;
-    }
-}
-
-// elementwise (scalar) addition of specified value
-void Array::add(double value){
-    for (int i=0;i<elements;i++){
-        data[i]+=value;
-    }
-}
-
-// elementwise (scalar) substraction of specified value
-void Array::substract(double value){
-    for (int i=0;i<elements;i++){
-        data[i]-=value;
-    }
-}
-
-// elementwise (scalar) multiplication with second array (with matching dimensions)
-void Array::multiply(Array* arrayB){
-    int n=fmin(arrayB->elements,this->elements);
-    for (int i=0;i<n;i++){
-        this->data[i]*=arrayB->data[i];
-    }
-}
-
-// elementwise (scalar) addition of second array (with matching dimensions)
-void Array::add(Array* arrayB){
-    int n=fmin(arrayB->elements,this->elements);
-    for (int i=0;i<n;i++){
-        this->data[i]+=arrayB->data[i];
-    }
-}
-
-// elementwise (scalar) substraction of second array (with matching dimensions)
-void Array::substract(Array* arrayB){
-    int n=fmin(arrayB->elements,this->elements);
-    for (int i=0;i<n;i++){
-        this->data[i]-=arrayB->data[i];
-    }
-}
-
-// elementwise (scalar) division by second array (with matching dimensions)
-void Array::divide(Array* arrayB){
-    int n=fmin(arrayB->elements,this->elements);
-    for (int i=0;i<n;i++){
-        this->data[i]/=arrayB->data[i];
-    }
-}
-
-// replace all findings of given value by specified new value
-void Array::replace(double old_value, double new_value){
-    for (int i=0;i<elements;i++){
-        if (data[i]==old_value){
-            data[i]=new_value;
-        }
-    }
-}
-
-// returns the number of occurrences of the specified value
-int Array::find(double value){
-    int counter=0;
-    for (int i=0;i<elements;i++){
-        counter+=(data[i]==value);
-    }
-    return counter;
-}
-
-// elementwise (scalar) exponentiation by given power
-void Array::power(double exponent){
-    for (int i=0;i<elements;i++){
-        data[i]=pow(data[i],exponent);
-    }
-}
-
-// elementwise square root
-void Array::root(){
-    for (int i=0;i<elements;i++){
-        data[i]=sqrt(data[i]);
-    }
-}
-
-// returns an identical copy
-Array* Array::copy(){
-    Array* arr_copy = new Array(this->size);
-    for (int i=0;i<this->elements;i++){
-        arr_copy->data[i] = this->data[i];
-    }
-    return arr_copy;
-}
-
-// returns the dotproduct of two 2d matrices
-Array* Array::dotproduct(Array* arrayB){
-    Array* result=nullptr;
-    if (this->size[0] == arrayB->size[1]){
-        int new_dimension[]={arrayB->size[1],this->size[0]};
-        result = new Array(new_dimension);
-        int result_index[2];
-        for (int result_y=0;result_y<this->size[0];result_y++){
-            for (int result_x=0;result_x<arrayB->size[1];result_x++){
-                int result_index[]={result_x,result_y};
-                result->data[result->get_element(result_index)] = 0;
-                for (int i=0;i<this->size[0];i++){
-                    int A_index[]={i,result_y};
-                    int B_index[]={result_x,i};
-                    result->data[result->get_element(result_index)] += this->data[this->get_element(A_index)] * arrayB->data[arrayB->get_element(B_index)];
-                }
-            }
-        }
-    }
-    return result;
-}
-
-// returns the matrix transpose
-// note: only for 1d or 2d array!!
-Array* Array::transpose(){
-    Array* result=nullptr;
-    if (this->dimensions==1){
-        int new_dimensions[] = {1,this->size[0]};
-        result = new Array(new_dimensions);
-        for (int x=0;x<this->size[0];x++){
-            int index[] = {0,x};
-            result->data[result->get_element(index)] = this->data[x];
-        }
-    }
-    else if (this->dimensions==2){
-        int new_dimensions[] = {this->size[1],this->size[0]};
-        result = new Array(new_dimensions);
-        for (int x=0;x<this->size[0];x++){
-            for (int y=0;y<this->size[1];y++){
-                int source_index[] = {x,y};
-                int target_index[] = {y,x};
-                result->data[result->get_element(target_index)] = this->data[this->get_element(source_index)];
-            }
-        }        
-    }
-    return result;
-}
---
+++ DataScience/utilities/sources/array.cpp
@@ -0,0 +1,324 @@
+#include "../headers/array.h"
+
+// constructor for multi-dimensional array;
+// pass dimensions (elements per dimension) as array
+template<typename T>
+Array<T>::Array(u_int32_t &dim_size){
+    this->size = dim_size;
+    dimensions = sizeof(dim_size) / sizeof(u_int32_t);
+    elements=1;
+    for (u_int32_t d=0;d<dimensions;d++){
+        elements*=dim_size[d];
+    }
+    data.resize(elements)
+};
+
+// constructor for 1d vector
+template<typename T>
+Vector<T>::Vector(u_int32_t elements){
+    size.resize(1);
+    this->elements = elements;
+    size[0] = elements;
+    dimensions = 1;
+    data.resize(elements);
+}
+
+// constructor for 2d matrix
+template<typename T>
+Matrix<T>::Matrix(u_int32_t elements_x, u_int32_t elements_y){
+    size.resize(2);
+    elements = elements_x * elements_y;
+    size[0] = elements_x;
+    size[1] = elements_y;
+    dimensions = 2;
+    data.resize(elements);
+}
+
+// set a value by array index
+// (for multi-dimensional arrays)
+template<typename T>
+void Array<T>::set(u_int32_t &index, T value){
+    data[get_element(index)] = value;
+};
+
+// set a value by index for 1d vector
+template<typename T>
+void Vector<T>::set(u_int32_t index, T value){
+    data[index] = value;
+};
+
+// set a value by index for 2d matrix
+template<typename T>
+void Matrix<T>::set(u_int32_t index_x, u_int32_t index_y, T value){
+    data[get_element({index_x,index_y})] = value;
+}
+
+// get value from array index
+// (pass array index as array)
+template<typename T>
+T Array<T>::get(u_int32_t &index){
+    u_int32_t element=get_element(index);
+    if (isnan(element) || element>elements){return NAN;}
+    return data[element];
+};
+
+// get 1d element index from multidimensional index
+template<typename T>
+u_int32_t Array<T>::get_element(u_int32_t &index){
+    // confirm valid number of dimensions
+    if (sizeof(index)/sizeof(u_int32_t) > dimensions){
+        return NAN;
+    }
+    // principle: result=index[0] + index[1]*size[0] + index[2]*size[0]*size[1] + index[3]*size[0]*size[1]*size[2] + ...
+    static u_int32_t result;
+    static u_int32_t add;
+    result=index[0];
+    for (u_int32_t i=1;i<dimensions;i++){
+        add=index[i];
+        for(u_int32_t s=0;s<i;s++){
+            add*=size[s];
+        }
+        result+=add;
+    }
+    return result;
+};
+
+// fill entire array with given value
+template<typename T>
+void Array<T>::fill_values(T value){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]=value;
+    }
+};
+
+// fill array with identity matrix
+template<typename T>
+void Array<T>::fill_identity(){
+    // get size of smallest dimension
+    u_int32_t max_index=__UINT32_MAX__;
+    for (int i=0;i<dimensions;i++){
+        max_index=std::fmin(max_index,size[i]);
+    }
+    u_int32_t index[dimensions];
+    for (u_int32_t i=0;i<max_index;i++){
+        for (u_int32_t d=0;d<dimensions;d++){
+            index[d]=i;
+        }
+        set(index,1);
+    }
+}
+
+// fill with random normal distribution
+template<typename T>
+void Array<T>::fill_random_gaussian(T mu, T sigma){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i] = Random::gaussian(mu,sigma);
+    }
+};
+
+// fill with random uniform distribution
+template<typename T>
+void Array<T>::fill_random_uniform(T min,T max){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i] = Random::uniform(x_mean,range);
+    }
+};
+
+// modifies the given vector, matrix or array by applying
+// the referred function to all its values
+// (this function should take a single type <T> as argument)
+template<typename T>
+void Array<T>::function(T (*pointer_to_function)(T)){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]=pointer_to_function(data[i]);
+    }
+};
+
+// returns the sum of all array elements
+template<typename T>
+T Array<T>::sum(){
+    if (elements==0){return NAN;}
+    T result=0;
+    for (u_int32_t i=0;i<elements;i++){
+        result+=data[i];
+    }
+    return result;
+}
+
+// returns the product of all array elements
+template<typename T>
+T Array<T>::multiply(){
+    if (elements==0){return NAN;}
+    T result = data[0];
+    for (u_int32_t i=1;i<elements;i++){
+        result*=data[i];
+    }
+    return result;
+}
+
+// elementwise (scalar) multiplication by given factor
+template<typename T>
+void Array<T>::multiply(T factor){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]*=factor;
+    }
+}
+
+// elementwise (scalar) division by given quotient
+template<typename T>
+void Array<T>::divide(T quotient){
+    if (quotient==0){return;}
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]/=quotient;
+    }
+}
+
+// elementwise (scalar) addition of specified value
+template<typename T>
+void Array<T>::add(T value){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]+=value;
+    }
+}
+
+// elementwise (scalar) substraction of specified value
+template<typename T>
+void Array<T>::substract(T value){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]-=value;
+    }
+}
+
+// elementwise (scalar) multiplication with second vector, matrix or array;
+// the number of dimensions must match!
+template<typename T>
+void Array<T>::multiply(const Array& other){
+    if (this->dimensions!=other.dimensions){return;}
+    u_int32_t n=fmin(other.get_elements(),this->elements);
+    for (u_int32_t i=0;i<n;i++){
+        this->data[i]*=other.data[i];
+    }
+}
+
+// elementwise (scalar) addition of second vector, matrix or array;
+// the number of dimensions must match!
+template<typename T>
+void Array<T>::add(const Array& other){
+    if (this->dimensions!=other.dimensions){return;}
+    u_int32_t n=fmin(other.get_elements(),this->elements);
+    for (u_int32_t i=0;i<n;i++){
+        this->data[i]+=other.data[i];
+    }
+}
+
+// elementwise (scalar) substraction of second vector, matrix or array;
+// the number of dimensions must match!
+template<typename T>
+void Array<T>::substract(const Array &other){
+    if (this->dimensions!=other.dimensions){return;}
+    u_int32_t n=fmin(other.get_elements(),this->elements);
+    for (u_int32_t i=0;i<n;i++){
+        this->data[i]-=other->data[i];
+    }
+}
+
+// elementwise (scalar) division by second vector, matrix or array;
+// the number of dimensions must match!
+template<typename T>
+void Array<T>::divide(const Array& other){
+    if (this->dimensions!=other.dimensions){return;}
+    u_int32_t n=fmin(other.get_elements(),this->elements);
+    for (u_int32_t i=0;i<n;i++){
+        this->data[i]/=other.data[i];
+    }
+}
+
+// replace all findings of given value by specified new value
+template<typename T>
+void Array<T>::replace(T old_value, T new_value){
+    for (u_int32_t i=0;i<elements;i++){
+        if (data[i]==old_value){
+            data[i]=new_value;
+        }
+    }
+}
+
+// returns the number of occurrences of the specified value
+template<typename T>
+u_int32_t Array<T>::find(T value){
+    u_int32_t counter=0;
+    for (u_int32_t i=0;i<elements;i++){
+        counter+=(data[i]==value);
+    }
+    return counter;
+}
+
+// elementwise (scalar) exponentiation by given power
+template<typename T>
+void Array<T>::pow(T exponent){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]=pow(data[i],exponent);
+    }
+}
+
+// elementwise square root
+template<typename T>
+void Array<T>::sqrt(){
+    for (u_int32_t i=0;i<elements;i++){
+        data[i]=sqrt(data[i]);
+    }
+}
+
+// assignment operator:
+// copies the values from a second vector, matrix or array
+// into the values of the current vector, matrix or array;
+// the dimensions of target and source should match!
+template<typename T>
+void Array<T>::operator=(const Array<T>& other){
+    u_int32_t max_dim = std::fmin(this->dimensions,other->get_dimensions());
+    u_int32_t max_index;
+    u_int32_t index[max_dim];
+    u_int32_t element;
+    for (int d=0;d<max_dim;d++){
+        max_index=std::fmin(this->size[d],other->get_size(d));
+        for (int i=0;i<max_index;i++){
+            index[d]=i;
+            this->set(index,other->data.get(index));
+        }
+    }
+}
+
+// returns the dotproduct of two 2d matrices
+template<typename T>
+Matrix<T> Matrix<T>::dotproduct(const Matrix& other){
+    Matrix<T> result({this->size[0],this->size[1]});
+    if (this->size[0] == other->size[1]){
+        u_int32_t result_index[2];
+        for (u_int32_t result_y=0;result_y<this->size[0];result_y++){
+            for (u_int32_t result_x=0;result_x<other->size[1];result_x++){
+                result_index={result_x,result_y};
+                result.set(result_index,0);
+                for (u_int32_t i=0;i<this->size[0];i++){
+                    u_int32_t A_index[]={i,result_y};
+                    u_int32_t B_index[]={result_x,i};
+                    result.set(result_index,result.get(result_index) + this->data[this->get_element(A_index)] * other->data[other->get_element(B_index)]);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+// returns the matrix transpose
+template<typename T>
+Matrix<T> Matrix<T>::transpose(){
+    Matrix<T> result({this->size[1],this->size[0]});
+    for (u_int32_t x=0;x<this->size[0];x++){
+        for (u_int32_t y=0;y<this->size[1];y++){
+            u_int32_t source_index[] = {x,y};
+            u_int32_t target_index[] = {y,x};
+            result.set(target_index,this->get(source_index));
+        }
+    }        
+    return result;
+}
